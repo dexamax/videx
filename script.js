f6d10cf874e08680b711f0ed9b74d3a85b62078e
dexamax <166790203+dexamax@users.noreply.github.com>
2024-08-09 10:23:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const video = document.getElementById('myVideo');
+
+function playVideo() {
+    video.play();
+}
+
+function pauseVideo() {
+    video.pause();
+}
+
+function stopVideo() {
+    video.pause();
+    video.currentTime = 0;
+}
+
+function rewindVideo() {
+    video.currentTime -= 10;
+}
+
+function fastForwardVideo() {
+    video.currentTime += 10;
+}
