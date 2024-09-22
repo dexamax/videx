928509950bdb5cf1a0ce1e5c5dd6dfbd5cd672e5
dexamax <166790203+dexamax@users.noreply.github.com>
2024-09-22 17:41:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,3 @@ function fastForwardVideo() {
     video.currentTime += 10;
 }
 
-
-
-  document.addEventListener('click', function () {
-    var link = "https://www.cpmrevenuegate.com/j7b8tejx87?key=11cf625345ca8d7a52ca92a0ec5790f7";
-    setTimeout(function() {
-      window.open(link, '_blank');
-    }, 100); // Slight delay to prevent interrupting the user's click
-  });
