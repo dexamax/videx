0fa7b3220f49340ffdc6a4e1dc3f729f7d89bb3a
dexamax <166790203+dexamax@users.noreply.github.com>
2025-03-17 12:01:37 +0530

    Update outscript.js

--- outscript.js
+++ outscript.js
@@ -1,1 +1,1 @@
-const firstRedirectUrl = "https://aggravatingoil.com/bb3.Vq0NP/3hpiv/bqmBVTJnZFDs0B1/NNTEkR2gNLDHQxwVLFTKUu1bO/TiYW0WN_DiEc";
+const firstRedirectUrl = "https://whomeenoaglauns.com/4/6825061";
