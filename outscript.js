dbdf895b09dc9edf6446a5d8493fafee5b2e6c12
dexamax <166790203+dexamax@users.noreply.github.com>
2025-03-20 09:44:35 +0530

    Update outscript.js

--- outscript.js
+++ outscript.js
@@ -1,1 +1,1 @@
-const firstRedirectUrl = "https://ptukseew.top/4/8442621";
+const firstRedirectUrl = "https://aggravatingoil.com/bE3RV/0fP.3rpqvEbqmHVdJpZoDb0O1xNgT/kz2VNeD-QVwfLBTwUX1ZO/T_Yf0iNmDLEe";
