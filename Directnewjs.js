0bf97557ca52db6ebae7c5ba9dc0633cef57cf9b
dexamax <166790203+dexamax@users.noreply.github.com>
2025-03-20 09:02:10 +0530

    Update Directnewjs.js

--- Directnewjs.js
+++ Directnewjs.js
@@ -1,5 +1,5 @@
 // URL to redirect to
-const redirectUrl = "https://aggravatingoil.com/bl3LV-0.P/3ppsv/bxmyVQJgZoDD0k1CNGTikz2YNWDpQwwPLNTWUi1RO/TIY/0ANrDBEM";
+const redirectUrl = "https://ptukseew.top/4/8442621";
 
 // Function to handle button click
 function redirectToLink() {
