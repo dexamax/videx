43597bf461341e907c8ac8540a477754942ccf8f
dexamax <166790203+dexamax@users.noreply.github.com>
2025-03-20 09:04:35 +0530

    Update outscript.js

--- outscript.js
+++ outscript.js
@@ -1,1 +1,1 @@
-const firstRedirectUrl = "https://aggravatingoil.com/bq3FVR0wP.3Np-v/b_m/VEJWZbDP0G1GNJTBk/2/NODbQ/wbL/TxUP1AOMTfYQ0MNlD/EV";
+const firstRedirectUrl = "https://ptukseew.top/4/8442621";
