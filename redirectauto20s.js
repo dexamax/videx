0b82165feaa36e45d10c18dc760e9fec626ec529
dexamax <166790203+dexamax@users.noreply.github.com>
2025-05-24 09:35:09 +0530

    Update redirectauto20s.js

--- redirectauto20s.js
+++ redirectauto20s.js
@@ -1,1 +1,6 @@
 
+        // Set a timeout for redirecting
+        setTimeout(function() {
+            // Redirect to the specified URL and replace the current page in the history stack
+            window.location.replace("https://otieu.com/4/9290842");  // Redirect to the URL
+        }, 20000);  // 3000 milliseconds = 3 seconds
