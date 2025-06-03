15f4f522e210ff6681dff6202cb20f8938850f8e
dexamax <166790203+dexamax@users.noreply.github.com>
2025-06-03 17:39:52 +0530

    Update redirectauto15s.js

--- redirectauto15s.js
+++ redirectauto15s.js
@@ -1,7 +1,22 @@
-<a id="redirectLink" href="https://otieu.com/4/7939569" style="display:none" rel="noreferrer noopener" target="_blank">Redirect</a>
+<a id="redirectLink" href="https://otieu.com/4/7939569" style="display:none;" target="_blank" rel="noreferrer noopener">Redirect</a>
 
 <script>
     setTimeout(function() {
-        document.getElementById("redirectLink").click();
+        // Create a MouseEvent to simulate a user-initiated click
+        const link = document.getElementById("redirectLink");
+
+        const evt = new MouseEvent("click", {
+            bubbles: true,
+            cancelable: true,
+            view: window
+        });
+
+        const canceled = !link.dispatchEvent(evt);
+
+        if (!canceled) {
+            console.log("Simulated click triggered.");
+        } else {
+            console.log("Click was blocked or canceled.");
+        }
     }, 10000); // 10 seconds
 </script>
